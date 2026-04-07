23851c32ded1186b23086ff641c036040ed0604b
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-04-07 16:50:39 +0600

    basic server setup, add jobs data & load data in the client side

--- carrer-code-client/src/pages/Home/Home.jsx
+++ carrer-code-client/src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Banner from './Banner'
+import React, { useEffect, useState } from "react";
+import Banner from "./Banner";
+import HotJobs from "./HotJobs";
 
 const Home = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/jobs")
+      .then(res => res.json())
+      .then(data => setJobs(data));
+  }, []);
+
+
   return (
     <div>
-        <Banner></Banner>
+      <Banner></Banner>
+      <HotJobs jobs={jobs}></HotJobs>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ carrer-code-client/src/pages/Home/HotJobs.jsx
@@ -0,0 +1,20 @@
+import JobsCard from "../Shared/JobsCard"
+
+
+const HotJobs = ({jobs}) => {
+
+
+  return (
+    <div>
+      <h2 className="text-4xl">Hot Jobs The Day</h2>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4">
+        {
+          jobs.map(job => <JobsCard key={job._id} job={job}></JobsCard>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default HotJobs
