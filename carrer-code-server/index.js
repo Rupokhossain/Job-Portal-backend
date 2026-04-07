decb73566f5cbb084011c2d09390fecef40ac93d
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-04-08 01:20:36 +0600

    Display Job data using Job Card,  Create Job detail Api and show detailed data

--- carrer-code-server/index.js
+++ carrer-code-server/index.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
 // middleware
@@ -38,6 +38,13 @@ async function run() {
       const cursor = jobCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobCollection.findOne(query)
+      res.send(result)
     })
       
 
